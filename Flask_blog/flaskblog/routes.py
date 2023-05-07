dcbfb65a00e225842c43dac7b001b5d50c6b5995
mspec <altius200@gmail.com>
2023-05-07 21:23:13 +0300

    new structure

--- Flask_blog/flaskblog/routes.py
+++ Flask_blog/flaskblog/routes.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, url_for, redirect, flash
-from forms import RegistrationForm, LoginForm
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
 
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
@@ -36,19 +35,18 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')  # flash is a one-time alert
+        flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
-
-@app.route("/newpassword")
-def newpassword():
-    return render_template('newpassword.html', title='Send New Password')
-
-if __name__ == '__main__':
-    app.run(debug=True)
